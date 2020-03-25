3b08000168bd13c6b6bbad0dc62dcf84817c8509
Saad Asif <saadasif2121@gmail.com>
2020-03-24 21:45:31 -0700

    http redirect

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HttpsRedirect from 'react-https-redirect';
 import App from './App';
 import 'antd/dist/antd.css';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <HttpsRedirect>
+    <App />
+  </HttpsRedirect>,
+  document.getElementById('root')
+);
