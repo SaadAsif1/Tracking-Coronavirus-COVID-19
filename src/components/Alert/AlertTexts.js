1d7b73731e0169437f723ceecb3d47e573be1a0c
Saad Asif <saadasif2121@gmail.com>
2020-04-14 01:21:45 -0700

    comma seperated numbers

--- src/components/Alert/AlertTexts.js
+++ src/components/Alert/AlertTexts.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 export default function AlertTexts({ data }) {
+  const formatNumber = (num) => {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+  };
+
   return (
-    <div className='containerText'>
-      <div>Total Cases: {data.cases}</div>
-      <div>Total Death: {data.deaths}</div>
-      <div>Total Recovered: {data.recovered}</div>
+    <div>
+      {data !== '' && (
+        <div className='containerText'>
+          <div>Total Cases: {formatNumber(data.cases)}</div>
+          <div>Total Death: {formatNumber(data.deaths)}</div>
+          <div>Total Recovered: {formatNumber(data.recovered)}</div>
+        </div>
+      )}
     </div>
   );
 }
